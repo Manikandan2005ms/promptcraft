8c651046391f55f0b8cd24d6269807a4cbf83087
Manikandan2005ms <mmani786ms@gmail.com>
2026-05-03 19:23:29 +0530

    fix build and deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: "/promptcraft/",
+  plugins: [react()]
 })
-export default {
-  base: "/promptcraft/"
-}
